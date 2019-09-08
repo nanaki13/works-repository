d049cdb077f294fdeb648567e7b2fa5029f32b22
nanaki13 <jonathan.bonnet@laposte.net>
2019-09-08 15:30:18 +0200

    tmp comment theme DAO

--- src/main/scala/bon/jo/helloworld/juliasite/pers/ThemeDAO.scala
+++ src/main/scala/bon/jo/helloworld/juliasite/pers/ThemeDAO.scala
@@ -1,22 +1,22 @@
-package bon.jo.helloworld.juliasite.pers
-
-import slick.dbio.{Effect, NoStream}
-import slick.jdbc.{H2Profile, JdbcProfile, PostgresProfile}
-
-trait Application extends RepositoryContext with Repository {
-
-}
-
-class ApplicationFrom(override val profile: JdbcProfile) extends Application {
-
-  override def profileName: String = "h2"
-  val _import = profile.api
-}
-
-object ApplicationInMemmoryH2 extends ApplicationFrom(H2Profile)
-
-class ApplicationPostgresProfile() extends ApplicationFrom(PostgresProfile){
-
-
-  override def profileName: String = "postgres"
-}
+//package bon.jo.helloworld.juliasite.pers
+//
+//import slick.dbio.{Effect, NoStream}
+//import slick.jdbc.{H2Profile, JdbcProfile, PostgresProfile}
+//
+//trait Application extends RepositoryContext with Repository {
+//
+//}
+//
+//class ApplicationFrom(override val profile: JdbcProfile) extends Application {
+//
+//  override def profileName: String = "h2"
+//  val _import = profile.api
+//}
+//
+//object ApplicationInMemmoryH2 extends ApplicationFrom(H2Profile)
+//
+//class ApplicationPostgresProfile() extends ApplicationFrom(PostgresProfile){
+//
+//
+//  override def profileName: String = "postgres"
+//}
